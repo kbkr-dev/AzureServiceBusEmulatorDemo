2ef2e9104c8e932909f5e25ac4b21ac8e10f4946
Bharath kumar reddy kummetha <kbkr033@gmail.com>
2025-01-25 00:04:51 +0530

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,94 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        private static string _connectionString = "Endpoint=sb://localhost:5672;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+
+        static async Task Main(string[] args)
+        {
+            await ConsumeQueue();
+            await ConsumeTopic();
+        }
+
+        private static async Task ConsumeTopic()
+        {
+            var topicName = "topic.1";
+            await ConsumeMessageFromSubscription(topicName, "subscription.1");
+            await ConsumeMessageFromSubscription(topicName, "subscription.2");
+            await ConsumeMessageFromSubscription(topicName, "subscription.3");
+        }
+
+        private static async Task ConsumeMessageFromSubscription(string topicName, string subscriptionName)
+        {
+            Console.WriteLine($"Rcv_Sub {subscriptionName} Begin");
+
+            var client1 = new ServiceBusClient(_connectionString);
+            var opt1 = new ServiceBusProcessorOptions();
+            opt1.ReceiveMode = ServiceBusReceiveMode.PeekLock;
+            var processor1 = client1.CreateProcessor(topicName, subscriptionName, opt1);
+
+            processor1.ProcessMessageAsync += MessageHandler;
+            processor1.ProcessErrorAsync += ErrorHandler;
+
+            await processor1.StartProcessingAsync();
+
+            await Task.Delay(TimeSpan.FromSeconds(5));
+
+            await processor1.StopProcessingAsync();
+            await processor1.DisposeAsync();
+            await client1.DisposeAsync();
+            Console.WriteLine($"Rcv_Sub {subscriptionName} End");
+        }
+
+        private static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received message: SequenceNumber:{args.Message.SequenceNumber} Body:{body}");
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        private static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine($"Message handler encountered an exception {args.Exception}.");
+            return Task.CompletedTask;
+        }
+
+        private static async Task ConsumeQueue()
+        {
+            string queueName = "queue.1";
+
+            var client = new ServiceBusClient(_connectionString);
+
+            ServiceBusReceiverOptions opt = new ServiceBusReceiverOptions
+            {
+                ReceiveMode = ServiceBusReceiveMode.PeekLock,
+            };
+
+            ServiceBusReceiver receiver = client.CreateReceiver(queueName, opt);
+
+            while (true)
+            {
+                ServiceBusReceivedMessage message = await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(5));
+                if (message != null)
+                {
+                    Console.WriteLine($"Received message: {Encoding.UTF8.GetString(message.Body)}");
+
+                    await receiver.CompleteMessageAsync(message);
+                }
+                else
+                {
+                    Console.WriteLine("No messages received.");
+                    break;
+                }
+            }
+
+            Console.WriteLine("Done recieving");
+
+            await receiver.DisposeAsync();
+            await client.DisposeAsync();
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,75 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text;
+
+namespace Publisher
+{
+    internal class Program
+    {
+        private static string _connectionString = "Endpoint=sb://localhost:5672;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+        static async Task Main(string[] args)
+        {
+            await PublishMessageToQueue();
+            await PublishMessageToTopic();
+        }
+
+        private static async Task PublishMessageToQueue()
+        {
+            const int numOfMessagesPerBatch = 10;
+            const int numOfBatches = 10;
+
+            string queueName = "queue.1";
+
+            var client = new ServiceBusClient(_connectionString);
+            var sender = client.CreateSender(queueName);
+
+            for (int i = 1; i <= numOfBatches; i++)
+            {
+                using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+                for (int j = 1; j <= numOfMessagesPerBatch; j++)
+                {
+                    messageBatch.TryAddMessage(new ServiceBusMessage($"Batch:{i}:Message:{j}, sample message"));
+                }
+                await sender.SendMessagesAsync(messageBatch);
+            }
+
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+
+            Console.WriteLine($"{numOfBatches} batches with {numOfMessagesPerBatch} messages per batch has been published to the queue.");
+        }
+
+        private static async Task PublishMessageToTopic()
+        {
+            var topicName = "topic.1";
+
+            await using (var client = new ServiceBusClient(_connectionString))
+            {
+                ServiceBusSender sender = client.CreateSender(topicName);
+
+                //First 50 message will goto Subscription 1 and Subscription 3 as per set filters in Config.json
+                for (int i = 1; i <= 50; i++)
+                {
+                    ServiceBusMessage message = new ServiceBusMessage(Encoding.UTF8.GetBytes($"Message number : {i}"))
+                    {
+                        ContentType = "application/json"
+                    };
+
+                    await sender.SendMessageAsync(message);
+                }
+
+                //Next 50 message will goto Subscription 2 and Subscription 3 as per set filters  in Config.json
+
+                for (int i = 51; i <= 100; i++)
+                {
+                    ServiceBusMessage message = new ServiceBusMessage(Encoding.UTF8.GetBytes($"Message number : {i}"));
+                    message.ApplicationProperties.Add("prop1", "value1");
+
+                    await sender.SendMessageAsync(message);
+                }
+            }
+
+            Console.WriteLine("Sent 100 messages to the topic.");
+        }
+    }
+}
